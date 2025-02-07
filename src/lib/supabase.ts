720ca630d0b9b0cbe7afc731d242912a96c0742e
CyzmiX <itzmedigamingx@gmail.com>
2025-02-07 21:00:39 +0100

    token refresh 4

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
-import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
 
-const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY, {
+const supabaseUrl = 'https://sgvkmwnesmllzgmdpddw.supabase.co';
+const supabase = createClient(supabaseUrl, import.meta.env.VITE_SUPABASE_KEY, {
     auth: {
         persistSession: true,
         autoRefreshToken: true,
